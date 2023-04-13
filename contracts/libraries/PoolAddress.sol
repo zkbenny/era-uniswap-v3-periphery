9b970708af97d5f85a1141dfeff47f944ebbbe63
AntonD3 <antonv.dyadyuk@gmail.com>
2023-04-13 20:15:32 +0100

    Update address derivation for PoolAddress

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -36,10 +36,11 @@ library PoolAddress {
             uint256(
                 keccak256(
                     abi.encodePacked(
-                        hex'ff',
-                        factory,
-                        keccak256(abi.encode(key.token0, key.token1, key.fee)),
-                        POOL_INIT_CODE_HASH
+                        bytes32(0x2020dba91b30cc0006188af794c2fb30dd8520db7e2c088b7fc7c103c00ca494), // keccak256("zksyncCreate2")
+                        bytes32(uint256(uint160(factory))), // sender
+                        keccak256(abi.encode(key.token0, key.token1, key.fee)), // salt
+                        POOL_INIT_CODE_HASH, // bytecode hash
+                        bytes32(0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470) // constructor input hash: keccak256("")
                     )
                 )
             )
