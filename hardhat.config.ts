4c1279085d704574dfbe73de29986d9198a8aa9a
AntonD3 <antonv.dyadyuk@gmail.com>
2023-04-28 18:54:10 +0100

    Update zksolc, verify plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import '@nomiclabs/hardhat-waffle'
 import '@nomiclabs/hardhat-etherscan'
 import 'hardhat-watcher'
 import '@matterlabs/hardhat-zksync-solc'
+import '@matterlabs/hardhat-zksync-verify'
 import { subtask } from 'hardhat/config'
 import { ZkSolcConfig } from '@matterlabs/hardhat-zksync-solc/dist/src/types'
 import * as path from 'path'
@@ -14,32 +15,29 @@ const CONTRACTS_USES_LIBRARIES = [
   'test/NFTDescriptorTest.sol',
 ];
 
-let zksolc_config: ZkSolcConfig = {
-  version: "1.3.8",
+// TODO: replace with ZkSolcConfig after hardhat-zksync-solc update
+let zksolc_config: any = {
+  version: "1.3.10",
   compilerSource: "binary",
-  settings: {},
-};
-
-if(process.env.NFT_DESCRIPTOR_ADDRESS) {
-  zksolc_config.settings.libraries = {
-    "contracts/libraries/NFTDescriptor.sol": {
-      NFTDescriptor: process.env.NFT_DESCRIPTOR_ADDRESS,
+  settings: {
+    metadata: {
+      bytecodeHash: 'none',
     },
-  }
-} else {
-  subtask(
-      TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS,
-      async (_, { config }, runSuper) => {
-        const paths = await runSuper();
-
-        return paths
-            .filter((solidityFilePath: any) => {
-              const relativePath = path.relative(config.paths.sources, solidityFilePath);
-              return !CONTRACTS_USES_LIBRARIES.includes(relativePath);
-            })
-      }
-  );
+  },
 }
+
+subtask(
+  TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS,
+  async (_, { config }, runSuper) => {
+    const paths = await runSuper()
+
+    return paths
+      .filter((solidityFilePath: any) => {
+        const relativePath = path.relative(config.paths.sources, solidityFilePath)
+        return !CONTRACTS_USES_LIBRARIES.includes(relativePath)
+      })
+  }
+)
 
 export default {
   networks: {
@@ -69,6 +67,7 @@ export default {
       url: "https://testnet.era.zksync.dev",
       ethNetwork: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
       zksync: true,
+      verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification'
     },
   },
   etherscan: {
@@ -81,7 +80,6 @@ export default {
     settings: {
       optimizer: {
         enabled: true,
-        // will be ignored by zksolc
         runs: 1_000_000,
       },
       metadata: {
